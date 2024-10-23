507c0196ce29a0054db91de5c9779d347906e04b
hi <Ezekenny91@gmail.com>
2024-10-23 07:48:40 -0700

    updated login form

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -5,18 +5,22 @@ import {Link} from "react-router-dom"
 const login = () => {
   return (
     <div> 
-        <div className='login-container d-flex justify-content-center align-items-center vh-70'>
+        <div className='login-container d-flex justify-content-center align-items-center vh-100'>
             <div className="row">
               <div className="col-12">
-                <div className="login-card">
-                  <h3>Login</h3>
+
+                <div className='d-flex align-items-center justify-content-center pb-4'>
+                  <h3 className='bg-black p-2 text-white rounded-2'>SHOPIT</h3>
+                </div>
+                <div className="login-card d-flex flex-column justify-content-center align-items-center ">
+                  <h3 className='mb-4'>Welcome Back</h3>
 
                   <form action="" className='login-form d-flex flex-column gap-3'>
                     
                     <div >
                       <input 
                         type="email" 
-                        className="input-form" 
+                        className="login-input" 
                         name='email' 
                         placeholder='email'
                       />
@@ -25,19 +29,26 @@ const login = () => {
                     <div>
                       <input 
                         type="password" 
-                        className="input-form" 
+                        className="login-input" 
                         name='password' 
                         placeholder='password' 
                       />
+
+                      <div>
+                       <Link to='/forgot-password' className='text-link-color mt-2'>Forgot Password</Link>
+                      </div>
                     </div>
                     <div>
-                      <Link to='/forgot-password'>Forgot Password</Link>
+                      
 
-                      <div className='d-flex justify-content-center align-items-center gap-4'>
+                      <div className='d-flex justify-content-center align-items-center gap-5'>
                          <button className='login-btn'>Login</button>
-                         <Link className= 'signup-btn'>Signup</Link>
-
                       </div>
+                        <p className='mt-2 d-flex justify-content-center align-items-center'>
+                          Don't have an account? 
+                          <Link className= 'signup-link text-link-color ps-1'>Signup Now</Link>
+                        </p>                   
+
                     </div>
               
                   </form>
